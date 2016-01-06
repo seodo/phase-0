51df70a5d2891a0084de7e90483eef8fb2774c18
John Seo <xjohnx92@hotmail.com>
2016-01-05 22:42:13 -0500

    what?

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -9,11 +9,25 @@
 # 0. Pseudocode
 
 # What is the input?
+# Input is a positive integer
 # What is the output? (i.e. What should the code return?)
+# Output is a comma-separated integer as a string.
 # What are the steps needed to solve the problem?
+# Convert integer into string
+# Determine number of characters, and if it is 3 or fewer digits, return as is.
+# If it has 4 or more digits, insert comma after the 3rd, 6th, 9th, etc position (counting from the right)
+
 
 
 # 1. Initial Solution
+def separate_comma(num)
+	if num.length <= 3
+		return num
+	end
+	index = -4
+	array = []
+	array = num.to_s.split("")
+	while 
 
 
 
